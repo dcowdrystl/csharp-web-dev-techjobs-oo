c2bdcac41bbb7bb8423c4a838e3af4609ddedeeb
Dan Cowdry <medtc411@gmail.com>
2021-12-01 08:06:10 -0800

    TestSettingJobId completed

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -11,5 +11,16 @@ namespace TechJobsTests
         {
             Assert.AreEqual(10, 10, 0.001);
         }
+
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+
+            Job job1 = new Job();
+            Job job2 = new Job();
+            Assert.IsTrue(job1.Id != job2.Id && (job1.Id + 1) == job2.Id);
+            /*int id = (job1.Id + 1);
+            Assert.IsTrue(id == job2.Id);*/
+        }
     }
 }
