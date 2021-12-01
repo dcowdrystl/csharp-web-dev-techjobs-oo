890572485368676275af75237996dc521c03ffaa
Dan Cowdry <medtc411@gmail.com>
2021-12-01 07:18:37 -0800

    added constructor in Location

--- TechJobsOO/PositionType.cs
+++ TechJobsOO/PositionType.cs
@@ -13,11 +13,29 @@ namespace TechJobsOO
             nextId++;
         }
 
+
         public PositionType(string value) : this()
         {
             Value = value;
         }
-
         // TODO: Add custom Equals(), GetHashCode(), and ToString() methods.
+
+        public override bool Equals(object obj)
+        {
+            return obj is PositionType type &&
+                   Id == type.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        
     }
 }
