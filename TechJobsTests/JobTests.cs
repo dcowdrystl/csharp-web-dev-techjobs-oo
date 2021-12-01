367616e952c4d7de75548a7564ea26ed1f4fdb80
Dan Cowdry <medtc411@gmail.com>
2021-12-01 07:45:46 -0800

    added techjobstests project and first empty test

---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, 0.001);
+        }
+    }
+}
